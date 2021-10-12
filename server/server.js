027b8a685492c14d061b665d0647cd965d1a5d64
AdhikarShinde <adhikar.shinde@coditas.com>
2021-10-12 13:10:15 +0530

    Setup Production Build and Server

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const { response } = require('express');
 const express = require('express');
 const path  = require('path');
 const app = express();
-const port = server.listen(process.env.Port) || 3000;
+const port = (process.env.Port) || 3000;
 const publicPath = path.join(__dirname,'..','public')
 
 app.use(express.static(publicPath));

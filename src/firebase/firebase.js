80caa84c4ac1ff27d307fe0403d32181ffee40e4
AdhikarShinde <adhikar.shinde@coditas.com>
2021-10-14 16:37:45 +0530

    Added Expense to Firebase ,created test db

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,22 +1,93 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDKqJ9DfbFEa57OisMsgCsWBisSYjV3u4E",
-    authDomain: "expensify-e3b85.firebaseapp.com",
-    databaseURL: "https://expensify-e3b85-default-rtdb.firebaseio.com",
-    projectId: "expensify-e3b85",
-    storageBucket: "expensify-e3b85.appspot.com",
-    messagingSenderId: "263045640302",
-    appId: "1:263045640302:web:dfdde4bb05b0e8858b767f",
-    measurementId: "G-2WMPDTZ15K"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PEOJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGEING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
 
-database.ref().on('value', (snapshot) => {
-    console.log(snapshot.val())
-})
+export { firebase, database as default };
+
+// // Child Added
+// database.ref('Expenses').on('child_added', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val())
+// })
+// // Child Changed
+// database.ref('Expenses').on('child_changed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val())
+// })
+//Child Removed
+// database.ref('Expenses').on('child_removed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val())
+// })
+
+// database.ref('Expenses')
+//     .on('value',((snapshot) => {
+//         const expenses = [];
+//         snapshot.forEach(child => {
+//             expenses.push({
+//                 id: child.key,
+//                 ...child.val()
+//             });
+//         });
+//         console.log(expenses)
+//     }));
+// database.ref('Expenses')
+//         .once('value')
+//         .then((snapshot) => {
+//             const expenses = [];
+
+//             snapshot.forEach(child => {
+//                 expenses.push({
+//                     id: child.key,
+//                     ...child.val()
+//                 });
+//             });
+// console.log(expenses)
+//         });
+
+// database.ref('Expenses').push({
+//     description:'Coffee',
+//     note: "Tried Expensive Coffee",
+//     amount: 25.69,
+//     createdAt: "12/12/2021"
+// })
+// database.ref('Expenses').push({
+//     description:'Rent',
+//     note: "",
+//     amount: 190515,
+//     createdAt: "19/10/2021"
+// })
+// database.ref('Expenses').push({
+//     description:'Water',
+//     note: "Monthly Water Bill",
+//     amount: 59.32,
+//     createdAt: "30/10/2021"
+// })
+
+
+
+// database.ref('notes/-Mlx2WOa6vjg-gQiRQLR').remove()
+// database.ref('notes').push({
+//     title:'Course Topics',
+//     body: "React Native , Node , Python"
+// })
+
+//  Challange
+// database.ref().on('value', (snapshot) => {
+//     const val = (snapshot.val())
+//     console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`)
+// })
+
+
 
 // database.ref('location/Country')
 //         .once('value')

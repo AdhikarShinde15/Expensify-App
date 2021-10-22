27f608e2247da096c601b64102af36b6715f625d
AdhikarShinde <adhikar.shinde@coditas.com>
2021-10-22 18:08:26 +0530

    Completed the Styling

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import {Header} from '../../components/Header';
-import { startLogin } from '../../actions/auth';
+
 
 test('should render Header correctly', () => {
   const wrapper = shallow(<Header startLogout= {() => { }}/>);
